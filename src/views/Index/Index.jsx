8ed44e965d51a8de1e4ab8c39d2eb84251f6e36e
liyuanV <17610615030@163.com>
2022-02-18 17:44:32 +0800

    feat: index-link

--- src/views/Index/Index.jsx
+++ src/views/Index/Index.jsx
@@ -49,24 +49,24 @@ export default function Index(props) {
         <div className={"wrap"}>
             <div className={"cover-box"}>
                 <img className={"cover"} src={cover1}/>
-                <a className={"nav-btn invite"} href='' ></a>
-                <a className={"nav-btn commands"} href='' ></a>
-                <a className={"nav-btn faq"} href='' ></a>
-                <a className={"nav-btn community"} href='' ></a>
-                <a className={"invite-btn"} href='' ></a>
+                <a className={"nav-btn invite"} href='https://discord.com/api/oauth2/authorize?client_id=928559137179172874&permissions=8&scope=bot' ></a>
+                <a className={"nav-btn commands"} href='https://www.notion.so/bepopula/Command-Glossary-78b5c08de0ee4c399e646976866be6a4' ></a>
+                <a className={"nav-btn faq"} href='https://www.notion.so/bepopula/Discord-NepBot-Knowledge-Base-78abb8b9570b4e1f8bedd8cdf912d055' ></a>
+                <a className={"nav-btn community"} href='https://discord.gg/QdQdHm5Tvm' ></a>
+                <a className={"invite-btn"} href='https://discord.com/api/oauth2/authorize?client_id=928559137179172874&permissions=8&scope=bot' ></a>
             </div>
             <div className={"cover-box"}>
                 <img className={"cover"} src={cover2}/>
                 <div className={"cover-start-box"}>
                     <img className={"cover"} src={cover_start}/>
-                    <a className={"start-btn"} href='' ></a>
+                    <a className={"start-btn"} href='https://discord.com/api/oauth2/authorize?client_id=928559137179172874&permissions=8&scope=bot' ></a>
                 </div>
             </div>
             <div className={"media-box"}>
                 <img className={"cover"} src={media}/>
-                <a className={"twitter"} href=""></a>
-                <a className={"github"} href=""></a>
-                <a className={"discord"} href=""></a>
+                <a className={"twitter"} href="https://twitter.com/nepbot4near"></a>
+                <a className={"github"} href="https://github.com/NepBot"></a>
+                <a className={"discord"} href="https://discord.gg/QdQdHm5Tvm"></a>
             </div>
             
             {/* <div className={'content'}>
