eae4e63b24a408084be76c8f3320d01c5dddc1bb
bhc8521 <bhc8521>
2022-02-21 23:13:13 +0800

    fix: links

--- src/views/Index/Index.jsx
+++ src/views/Index/Index.jsx
@@ -10,7 +10,8 @@ import cover1 from '../../assets/imgs/index/cover1.svg';
 import cover2 from '../../assets/imgs/index/cover2.svg';
 import cover_start from '../../assets/imgs/index/cover_start.svg';
 import media from '../../assets/imgs/index/media.svg';
-
+import power from '../../assets/imgs/powered_by.svg';
+import near_logo from '../../assets/imgs/near_logo.svg';
 
 export default function Index(props) {
 
@@ -18,26 +19,30 @@ export default function Index(props) {
         <div className={"wrap"}>
             <div className={"cover-box"}>
                 <img className={"cover"} src={cover1}/>
-                <a className={"nav-btn invite"} href='https://discord.com/api/oauth2/authorize?client_id=928559137179172874&permissions=8&scope=bot' ></a>
-                <a className={"nav-btn commands"} href='https://www.notion.so/bepopula/Command-Glossary-78b5c08de0ee4c399e646976866be6a4' ></a>
-                <a className={"nav-btn faq"} href='https://www.notion.so/bepopula/Discord-NepBot-Knowledge-Base-78abb8b9570b4e1f8bedd8cdf912d055' ></a>
-                <a className={"nav-btn community"} href='https://discord.gg/QdQdHm5Tvm' ></a>
-                <a className={"invite-btn"} href='https://discord.com/api/oauth2/authorize?client_id=928559137179172874&permissions=8&scope=bot' ></a>
+                <a className={"nav-btn invite"} href='https://discord.com/api/oauth2/authorize?client_id=928559137179172874&permissions=8&scope=bot' target="view_window"></a>
+                <a className={"nav-btn commands"} href='https://nepbot.notion.site/Command-Glossary-e5ffdf2d51e24fc0a569d1cb3ef6e035' target="view_window"></a>
+                <a className={"nav-btn faq"} href='https://nepbot.notion.site/Discord-NepBot-Knowledge-Base-dc875fc6c3f84149aa8a76ef7a2a23ab' target="view_window"></a>
+                <a className={"nav-btn community"} href='https://discord.gg/QdQdHm5Tvm' target="view_window"></a>
+                <a className={"invite-btn"} href='https://discord.com/api/oauth2/authorize?client_id=928559137179172874&permissions=8&scope=bot' target="view_window"></a>
             </div>
             <div className={"cover-box"}>
                 <img className={"cover"} src={cover2}/>
                 <div className={"cover-start-box"}>
                     <img className={"cover"} src={cover_start}/>
-                    <a className={"start-btn"} href='https://discord.com/api/oauth2/authorize?client_id=928559137179172874&permissions=8&scope=bot' ></a>
+                    <a className={"start-btn"} href='' ></a>
                 </div>
             </div>
             <div className={"media-box"}>
                 <img className={"cover"} src={media}/>
-                <a className={"twitter"} href="https://twitter.com/nepbot4near"></a>
-                <a className={"github"} href="https://github.com/NepBot"></a>
-                <a className={"discord"} href="https://discord.gg/QdQdHm5Tvm"></a>
+                <a className={"twitter"} href="https://twitter.com/nepbot4near" target="view_window"></a>
+                <a className={"github"} href="https://github.com/NepBot" target="view_window"></a>
+                <a className={"discord"} href="https://discord.gg/QdQdHm5Tvm" target="view_window"></a>
             </div>
-            
+            <div className={"power-box"}>
+                <img src={power}/>
+                <img src={near_logo}/>
+                <a href="https://near.org" target="view_window"></a>
+            </div>
             {/* <div className={'content'}>
                 <div className={'text'}>
                     vera
