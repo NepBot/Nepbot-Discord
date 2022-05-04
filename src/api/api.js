ccf44a097f74eab186337c09d00732a7eab9d1a8
bhc8521 <bai_haochen@163.com>
2022-05-04 18:04:14 +0800

    fix

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,11 @@
-import {axios} from "./requst";
-export const setInfo = async (data)=>{
-    return await axios.request({
-        method:"post",
-        url:`/set-info`,
-        data,
+
+export const setInfo = async (args) => {
+    const json = await fetch(`/api/set-info`, {
+        headers:{ 'Content-Type': 'application/json' },
+        method:"POST",
+        body:typeof args === 'string'?args:JSON.stringify(args)
     })
+    return await json.json()
 }
 export const signRule = async (args)=>{
     const json = await fetch(`/api/sign`,{
@@ -20,6 +21,7 @@ export const getRoleList = async (guild_id)=>{
 }
 export const getServer = async (guild_id)=>{
     const json = await fetch(`/api/getServer/${guild_id}`);
+    console.log(json.json())
     return await json.json();
 }
 export const getUser = async (guild_id, user_id) => {
