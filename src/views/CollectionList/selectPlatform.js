15bc4fe5d7da1108c1204fb1c458f55ae915f060
olina-ly <17610615030@163.com>
2022-10-27 19:13:57 +0800

    add sass

---
+++ src/views/CollectionList/selectPlatform.js
@@ -0,0 +1,16 @@
+import React,{useState} from 'react';
+// import {useHistory} from 'react-router-dom'
+// import {connect, WalletConnection} from "near-api-js";
+// import {signRule,createCollection, getCollection} from "../../api/api";
+// import {contract, parseAmount, sign} from "../../utils/util";
+import './selectPlatform.scss'
+
+function SelectPlatform(props) {
+    return <div className="select-platform">
+        <div className="select-content">
+
+        </div>
+    </div>;
+}
+
+export default SelectPlatform;
