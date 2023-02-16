2cfc3e4ba2b0800d1bd5b476e2b4ec573a5fa3df
Hikaru Chang <i@rua.moe>
2023-02-17 04:08:15 +0800

    update

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -2,8 +2,29 @@
  * @ Author: Hikaru
  * @ Create Time: 2023-02-08 02:50:33
  * @ Modified by: Hikaru
- * @ Modified time: 2023-02-08 02:51:33
+ * @ Modified time: 2023-02-16 23:41:24
  * @ Description: i@rua.moe
  */
 
+import { arbitrum, mainnet, polygon, goerli } from 'wagmi/chains';
+
 export const expDate = 1669910399;
+
+const isDev: boolean = true;
+
+export const WALLETCONNECT = {
+  projectID: 'e955aa92a6d9db97160d53e9afc53f5d',
+  appName: 'Nepbot',
+  chains: [arbitrum, mainnet, polygon, goerli],
+  chainsID: [arbitrum.id, mainnet.id, polygon.id, goerli.id],
+  universalChains: ["eip155:80001"],
+  methods: ['eth_sendTransaction', 'eth_signTransaction', 'eth_sign', 'personal_sign', 'eth_signTypedData'],
+  events: ['chainChanged', 'accountsChanged', 'connect', 'session_event', 'display_uri', 'disconnect'],
+  metadata: {
+    name: "Nepbot",
+    description: "Nepbot for WalletConnect",
+    url: "https://Nepbot.io/",
+    icons: ["https://avatars.githubusercontent.com/u/37784886"],
+  },
+  logger: "info",
+}
